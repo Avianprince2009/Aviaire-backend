65a53a85cbdab8a11194816cf4517cf78e253ba9
dev femzy <abubakriluqman7@gmail.com>
2026-05-22 09:55:36 +0100

    `Increased JSON payload limit from 1mb to 100mb`

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.use(
 
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
