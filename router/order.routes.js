ba8d7a27d6c10df297943237feecea6842bd2f2c
dev femzy <abubakriluqman7@gmail.com>
2026-06-02 10:51:31 +0100

    orders

--- router/order.routes.js
+++ router/order.routes.js
@@ -11,9 +11,11 @@ import {
 const router = express.Router();
 
 router.get("/orders", verifyUser, requireAdmin, getOrders);
+router.get("/admin/orders", verifyUser, requireAdmin, getOrders);
 router.get("/orders/:id", verifyUser, requireAdmin, getOrderById);
 router.patch("/orders/:id/status", verifyUser, requireAdmin, updateOrderStatus);
 router.delete("/orders/:id", verifyUser, requireAdmin, deleteOrder);
+
 
 export default router;
 
